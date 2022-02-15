1fa6f3c1df28f4e859e5c14344421f96730e4fc5
MEsen1 <mehmet.esen.94@gmail.com>
2022-02-15 17:49:08 +0000

    update_view

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from unicodedata import name
 from django.urls import path
-from .views import home, todo_list,todo_add
+from .views import home, todo_list,todo_add,todo_update
 
 urlpatterns = [
     path('',home, name='home'),
-    path('todos',todo_list,name='todo_list'),
-    path('todo_add',todo_add,name='todo_add')
+    path('todos/',todo_list,name='todo_list'),
+    path('todo_add/',todo_add,name='todo_add'),
+    path('todo_update/<int:id>/',todo_update,name='todo_update'),
 ]
